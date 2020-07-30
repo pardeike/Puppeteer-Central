5d2b7765d076e1b3eab1c85a943708c264a936c4
Andreas Pardeike <andreas@pardeike.net>
2020-07-30 22:19:00 +0200

    bump to v1.0

--- src/tools.ts
+++ src/tools.ts
@@ -2,7 +2,7 @@ import routesRef from './hooks/routes'
 import { v4 as uuidv4 } from 'uuid'
 import { BSON } from 'bsonfy'
 
-const version = 'v0.3.1'
+const version = 'v1.0.0'
 
 let _firstTime = true
 const firstTime = () => {
